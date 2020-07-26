751fb4fe757b505e39871d9edfe4b4f90e4ce211
Shane Koehler <shane.koehler@gmail.com>
2020-07-26 16:20:14 -0500

    setup env file for session secret and heroku

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
 
+// env setup
+require('dotenv').config(); // utilizes vars.env in root
+var PORT = process.env.PORT || 8080;
+
+// requires
 var express = require("express");
 var session = require("express-session");
 var exphbs = require("express-handlebars");
 var passport = require("./config/passport");
 var db = require("./models");
 
-var genData = require("./debug/generateFakeData"); // DEBUG
+// DEBUG
+var genData = require("./debug/generateFakeData"); 
 
-var PORT = process.env.PORT || 8080;
-
+// express setup
 var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(session({ secret: "ju#s82$bd82g0!&*@$#$FG^*@SAn&2", resave: true, saveUninitialized: true }));
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Requiring our routes
+// route handling
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-// Syncing our database and logging a message to the user upon success
+// sync db and start
 db.sequelize.sync({ force: true }).then(function() {
   genData();
   app.listen(PORT, function() {
